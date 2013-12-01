bbe97c212309ba657b0e2b9f44d013b1bb695ad6
Michael Hawkins <mike@hawkinsunlimited.com>
2013-11-30 20:24:54 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngBlog::Application.config.secret_key_base = '3f10f6c7a3e5fc9336da4f1f08ac6a767eeac775befa825f6b0910bbb158bf13821b1245140b834a8cc0a9b7eb5884764db1bf3908b898697167f29cd65e19f2'
